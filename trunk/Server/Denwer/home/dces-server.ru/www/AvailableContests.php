9779c6ddef22e6ee8a3a2e16ddfb96ea49a22450
iposov@gmail.com <iposov@gmail.com@7cf9c0ce-a34d-0410-a4ad-6744d85870b3>
2008-11-21 16:04:26 +0000

    1. added param 'actualAnswer' to ServerReturnedNoAnswer class 2. first import of denwer DCES virtual host

---
+++ trunk/Server/Denwer/home/dces-server.ru/www/AvailableContests.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+require("Messages.php");
+
+function processAvailableContestRequest($request) {
+  $res->contests = array();
+
+	$c1 = new ContestDescription();
+	$c1->name = "Example contest #1";
+	$res->contests[] = $c1;
+
+	$c2 = new ContestDescription();
+	$c2->name = "Example contest #2";
+	$res->contests[] = $c2;
+
+  return $res;
+}
+
+?>
