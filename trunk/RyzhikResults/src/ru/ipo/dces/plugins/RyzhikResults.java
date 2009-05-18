65832a208c2d16ffc2ca96024fdb50ca02367f94
iposov <iposov@7cf9c0ce-a34d-0410-a4ad-6744d85870b3>
2009-05-18 16:48:11 +0000

    Added stuff to debug RyzhikTest

--- trunk/RyzhikResults/src/ru/ipo/dces/plugins/RyzhikResults.java
+++ trunk/RyzhikResults/src/ru/ipo/dces/plugins/RyzhikResults.java
@@ -12,9 +12,15 @@ import ru.ipo.dces.clientservercommunication.GetContestResultsResponse;
 import ru.ipo.dces.clientservercommunication.StopContestRequest;
 
 import javax.swing.*;
+import javax.swing.text.*;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.StyleSheet;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.*;
 import java.util.Arrays;
+import java.io.StringReader;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,6 +44,8 @@ public class RyzhikResults implements Plugin {
   public RyzhikResults(PluginEnvironment environment) {
     this.environment = environment;
 
+    environment.setTitle("Резюме");
+
     mainPanel = new JPanel();            
   }
 
@@ -53,7 +61,7 @@ public class RyzhikResults implements Plugin {
       GetContestResultsResponse response = server.doRequest(r);
       String[][] getResults = response.table[response.userLine];
 
-      //remove admin and participant info if there is some
+      //remove admin and pdarticipant info if there is some
 
       int removeFirstColumns = response.headers[0].equals("admin info") ? 2 : 1;
       results = new String[getResults.length - removeFirstColumns - 1][]; //1 column is removed from the end
@@ -76,7 +84,7 @@ public class RyzhikResults implements Plugin {
     this.interfaceView = interfaceView;
 
     if (interfaceView == InterfaceView.Nothing) {
-      environment.log("Ошибка в плагине оторажения результов, оратитесь к разработчикам", LogMessageType.Error);
+      environment.log("Ошибка в плагине отображения результов, оратитесь к разработчикам", LogMessageType.Error);
       return;
     }
 
@@ -90,8 +98,25 @@ public class RyzhikResults implements Plugin {
     //TODO нарисовать интферфейс с результатом
     //Метод getResultsTest() как бы выдает результаты (сейчас текстом)
     //mainPanel - панель, положить весь интерфейс на нее
-    JLabel label = new JLabel(getResultsText());
-    mainPanel.add(label);
+
+    /*JTextPane resultsText = new JTextPane();
+    try {
+      resultsText.setContentType("text/html");
+      resultsText.read(new StringReader(getResultsText()), null);
+      resultsText.setPage("http://localhost");
+    } catch (IOException e) {
+      //do nothing
+    }*/
+
+    JTextArea resultsText = new JTextArea(getResultsText());
+
+    resultsText.setEditable(false);
+    resultsText.setFont(resultsText.getFont().deriveFont((float)14));
+    JScrollPane scroll = new JScrollPane(resultsText);
+    mainPanel.setLayout(new GridLayout(1,1));
+    mainPanel.add(scroll);
+    
+    mainPanel.validate();
   }
 
   private void setInterfaceDuringContest() {
@@ -100,25 +125,177 @@ public class RyzhikResults implements Plugin {
     //чтобы завершить соревнование, пользоваться методом stopContest();
     JLabel label = new JLabel("Результаты соревнования можно посмотреть только после окончания соревнования.");
     JButton button = new JButton("Завершить досрочно");
-    mainPanel.add(label);
-    mainPanel.add(button);
+    mainPanel.setLayout(new BorderLayout());
+    mainPanel.add(label, BorderLayout.CENTER);
+    mainPanel.add(button, BorderLayout.SOUTH);
 
     button.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        stopContest();
+        //TODO отладить вывод подтверждения
+        if (JOptionPane.showConfirmDialog(null, "точно?", "заголовок", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION)
+          stopContest();
       }
     });
+
+    mainPanel.validate();
   }
 
   private String getResultsText() {
-    //TODO use variable results and set text
-    String ans = "";
-    int i = 1;
-    for (String[] result : results) {
-      ans += i + ": " + Arrays.toString(result);
-      i++;
+    /*String results[][] = {
+            {"++ ++?--", "+- ?-++?", "-- +-+-?", "?? +-+-?", "!0 +-++?"},
+            {"-0 ++-??", "+0 ++-+?", "++ --?++", "+- ++-?+", "-- +++??"},
+            {"++ ++?--", "+- ?-++?", "-- +-+-?", "?? +-+-?", "!0 +-++?"},
+            {"-0 ++-??", "+0 ++-+?", "++ --?++", "+- ++-?+", "-- +++??"},
+            {"++ ?+?--", "-- ?-++?", "-- +-+-?", "?? ?-+-?", "!0 +-++?"},
+            {"-0 ++-??", "+0 ++-+?", "++ --?++", "++ ?+-?+", "-- +++??"},
+    };*/
+    int d1, r1, w1;
+    d1 = 0;
+    r1 = 0;
+    w1 = 0;
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < 5; j++) {
+        if (results[i][j].charAt(1) == '0') d1++;
+        else {
+          if (results[i][j].charAt(1) == results[i][j].charAt(0)) r1++;
+          else w1++;
+        }
+      }
     }
-    return ans;
+
+    int d2, r2, w2;
+    d2 = 0;
+    r2 = 0;
+    w2 = 0;
+    for (int i = 2; i < 4; i++) {
+      for (int j = 0; j < 5; j++) {
+        if (results[i][j].charAt(1) == '0') d2++;
+        else {
+          if (results[i][j].charAt(1) == results[i][j].charAt(0)) r2++;
+          else w2++;
+        }
+      }
+    }
+
+    int d3, r3, w3;
+    d3 = 0;
+    r3 = 0;
+    w3 = 0;
+    for (int i = 4; i < 6; i++) {
+      for (int j = 0; j < 5; j++) {
+        if (results[i][j].charAt(1) == '0') d3++;
+        else {
+          if (results[i][j].charAt(1) == results[i][j].charAt(0)) r3++;
+          else w3++;
+        }
+      }
+    }
+
+    String ball[] = {" баллов", " балл", " балла", " балла", " балла", " баллов", " баллов", " баллов", " баллов", " баллов", " баллов"};
+    String text = "";/*"<html>" +
+            "<head>" +
+            "<meta http-equiv=\"Content-Type\" content=\"text/html\">" +
+            "<title>" + "результаты" + "</title>" +
+            "</head>" +
+    "<body><h1>Результаты</h1>";*/
+    text += "по теме Числа Вы набрали: " + (r1 - w1) + ball[Math.abs(r1 - w1)] + ".<br/>";
+    text += "по теме Функции Вы набрали: " + (r2 - w2) + ball[Math.abs(r2 - w2)] + ".<br/>";
+    text += "по теме Фигуры Вы набрали: " + (r3 - w3) + ball[Math.abs(r3 - w3)] + ".<br/>";
+
+    int t1, t2, t3;
+    t1 = Functions.getSecText(w1, r1, d1);   //text number for "Числа"
+    t2 = Functions.getSecText(w2, r2, d2);   //text number for "Функции"
+    t3 = Functions.getSecText(w3, r3, d3);   //text number for "Фигуры"
+
+    String SecText[][] = {{"По теме ", "По темам ", " знания неудовлетворительные. ", " знания неудовлетворительные. "},
+            {"По теме ", "По темам ", " знания превосходные. ", " знания превосходные. "},
+            {"Тема ", "Темы ", " не осовена. ", " не освоены. "},
+            {"По теме ", "По темам ", " знания удовлетворительные, но не устойчивые.", " знания удовлетворительные, но не устойчивые. "},
+            {"Тема ", "Темы ", " освоена плохо. ", " освоены плохо. "},
+            {"По теме ", "По темам ", " знания неудовлетворительные.", " знания неудовлетворительные."},
+            {"Тема ", "Темы ", " освоена очень хорошо.", " освоены очень хорошо."},
+            {"Тема ", "Темы ", " освоена слабо.", " освоены слабо."},
+            {"Темой ", "Темами ", " Вы владеете хорошо. ", " Вы владеете хорошо. "},
+            {"По теме ", "По темам ", " знания неустойчивые.", " знания неустойчивые. "}};
+//if text numbers are different
+    if (((t1 != t2) & (t2 != t3)) & (t1 != t3)) {
+      text += SecText[t1 - 1][0] + "Числа" + SecText[t1][2];
+      text += SecText[t2 - 1][0] + "Функции" + SecText[t2][2];
+      text += SecText[t3 - 1][0] + "Фигуры" + SecText[t3][2];
+    }
+
+//if text numbers are not different
+    else {
+      if ((t1 == t2) & (t1 == t3)) text += SecText[t1 - 1][1] + "Числа, Функции, Фигуры" + SecText[t1 - 1][3];
+      else {
+        if (t1 == t2)
+          text += SecText[t1 - 1][1] + "Числа, Функции" + SecText[t1 - 1][3] + SecText[t3 - 1][0] + "Фигуры" + SecText[t3 - 1][2];
+        if (t1 == t3)
+          text += SecText[t1 - 1][1] + "Числа, Фигуры" + SecText[t1 - 1][3] + SecText[t2 - 1][0] + "Функции" + SecText[t2 - 1][2];
+        if (t2 == t3)
+          text += SecText[t2 - 1][1] + "Функции, Фигуры" + SecText[t2 - 1][3] + SecText[t1 - 1][0] + "Числа" + SecText[t1 - 1][2];
+      }
+    }
+
+    float e[] = {0, 0, 0, 0, 0};
+    float p[] = {0, 0, 0, 0, 0};
+    float e0[] = {0, 0, 0, 0, 0};
+    float p0[] = {0, 0, 0, 0, 0};
+
+    for (int i = 0; i < 6; i++) {
+
+      for (int j = 0; j < 5; j++) {
+
+        for (int k = 0; k < 5; k++) {
+          if (results[i][j].charAt(3 + k) == '+') {
+            e0[k]++;
+            if (results[i][j].charAt(1) == results[i][j].charAt(0)) e[k]++;
+          }
+          if (results[i][j].charAt(3 + k) == '?') {
+            p0[k]++;
+            if (results[i][j].charAt(1) == results[i][j].charAt(0)) p[k]++;
+          }
+        }
+      }
+    }
+
+    String param[] = {"умением оперировать фактами",
+            "вычислительными умениями",
+            "приемами оперирования визуальными представлениями знаний",
+            "умением переформулировать задачи и переходить от одной формы представления знаний к другой",
+            "умением строить логические суждения и выводы "};
+
+    String paramText[] = {"Вы в совершенстве владеете",
+            "Вы хорошо владеете",
+            "Вероятнее всего, Вы хорошо владеете",
+            "Вероятнее всего, Вы не очень хорошо владеете",
+            "Вероятнее всего, Вы на удовлетворительном уровне владеете",
+            "Вероятнее всего, Вы на низком уровне владеете",
+            "Показали на низком уровне владение",
+            "Не владеете "};
+    int t[] = {0, 0, 0, 0, 0};
+    for (int k = 0; k < 5; k++) {
+      e[k] = Math.round((e[k] / e0[k]) * 100);
+      p[k] = Math.round((p[k] / p0[k]) * 100);
+      t[k] = Functions.getParamText(e[k], p[k]);
+      text += "Владение " + param[k] + ": " + e[k] + "%<br/><br/>";
+    }
+
+    boolean condition[] = {true, true, true, true, true};
+    for (int i = 0; i < 5; i++) {
+      for (int j = i + 1; j < 5; j++) {
+        if (t[i] == t[j]) {
+          condition[i] = false;
+          condition[j] = false;
+        }
+      }
+    }
+    for (int k = 0; k < 5; k++) {
+      text += paramText[t[k] - 1] + " ";
+      if (condition[k]) text += param[k] + ". ";
+//     else text+=
+    }
+    return text + "</body></html>";
   }
 
   private void stopContest() {
--- trunk/RyzhikTest/src/ru/ipo/dces/plugins/RyzhikPlugin.java
+++ trunk/RyzhikTest/src/ru/ipo/dces/plugins/RyzhikPlugin.java
@@ -14,6 +14,8 @@ import java.awt.*;
 import java.io.FilenameFilter;
 import java.io.File;
 
+import info.clearthought.layout.TableLayout;
+
 /**
  * Created by IntelliJ IDEA.
  * User: I. Posov
@@ -24,24 +26,61 @@ import java.io.File;
 public class RyzhikPlugin implements Plugin {
 
   private JPanel mainPanel;
-  private final PluginEnvironment environment;
+  private PluginEnvironment myEnvironment;
+  private JLabel statement;
 
-  private JLabel warningLabel;
-  private JButton resubmitSolutionButton;
+  private final int CELL_WIDTH = 36;
 
-  public RyzhikPlugin(PluginEnvironment environment) {
-    this.environment = environment;
+  public RyzhikPlugin(PluginEnvironment pe) {
+    myEnvironment = pe;
+    myEnvironment.setTitle(pe.getProblemName());
+    mainPanel = new JPanel();
 
-    initInterface();
-
-    //try initialize solution
+    //send 'init'
     try {
-      environment.submitSolution(
-              SubmissionUtils.setAction("init", new HashMap<String, String>())
+      myEnvironment.submitSolution(
+            SubmissionUtils.setAction("init", new HashMap<String, String>())
       );
     } catch (GeneralRequestFailureException e) {
-      //TODO to think what to do if failed to send init request!!!
+      //DO NOTHING
     }
+
+    //create size for tablelayout
+    double size[][] =
+            {{165, 50, 10, CELL_WIDTH, 10, CELL_WIDTH, 10, CELL_WIDTH, 10, CELL_WIDTH, 10, CELL_WIDTH, TableLayout.FILL},
+                    {25, 314, 20, 16, 10, 16, 10, 16, 10, 16, 10, 16, TableLayout.FILL}};
+    mainPanel.setLayout(new TableLayout(size));
+    //create label with image
+    statement = new JLabel();
+    statement.setIcon(new ImageIcon(getStatementGif()));
+    mainPanel.add(statement, "1, 1, 12, 1");
+    //create groups of radiobuttons
+    for (int i = 1; i < 6; i++) {
+
+      JLabel item = new JLabel("Пункт " + i);
+      mainPanel.add(item, "1, " + (2 * i + 1));
+      String symbol[] = {"+", "-", "0", "?", "!"};
+      JRadioButton radioButton[] = new JRadioButton[symbol.length];
+      ButtonGroup bg = new ButtonGroup();
+
+      for (int j = 0; j < symbol.length; j++) {
+        radioButton[j] = new JRadioButton(symbol[j]);
+        mainPanel.add(radioButton[j], "" + (2 * j + 3) + ", " + (2 * i + 1));
+        bg.add(radioButton[j]);
+        radioButton[j].addActionListener(new RadioButtonActionListener(i, radioButton[j], myEnvironment));
+      }
+    }
+  }
+
+  private String getStatementGif() {
+    File folder = myEnvironment.getProblemFolder();
+    String[] gifs = folder.list(new FilenameFilter() {
+      public boolean accept(File dir, String name) {
+        return name.endsWith(".gif");
+      }
+    });
+    //TODO assert there is exactly one gif
+    return folder.getAbsolutePath() + "/" + gifs[0];
   }
 
   public JPanel getPanel() {
@@ -56,106 +95,106 @@ public class RyzhikPlugin implements Plugin {
     //do nothing
   }
 
-  private void initInterface() {
-    environment.setTitle(environment.getProblemName());
+//  private void initInterface() {
+//    environment.setTitle(environment.getProblemName());
+//
+//    //create panels
+//    JPanel solutionPanel = new JPanel();
+//    JPanel warningPanel = new JPanel();
+//    JPanel selectionPanel = new JPanel();
+//    mainPanel = new JPanel();
+//
+//    warningPanel.setVisible(false);
+//
+//    //set layout for panels
+//    mainPanel.setLayout(new BorderLayout());
+//    solutionPanel.setLayout(new BorderLayout());
+//    warningPanel.setLayout(new BorderLayout());
+//    selectionPanel.setLayout(new GridLayout(5, 1));
+//
+//    //add panels to panels
+//    mainPanel.add(solutionPanel, BorderLayout.CENTER);
+//    mainPanel.add(warningPanel, BorderLayout.SOUTH);
+//    solutionPanel.add(selectionPanel, BorderLayout.SOUTH);
+//
+//    //fill solution panel
+//    JLabel statement = new JLabel();
+//
+//    File statementFolder = environment.getProblemFolder();
+//    String[] gif_files = statementFolder.list(new FilenameFilter() {
+//      public boolean accept(File dir, String name) {
+//        return name.endsWith(".gif");
+//      }
+//    });
+//
+//    if (gif_files == null || gif_files.length == 0)
+//      statement.setText("Не удалось загрузить решение, обратитесь к администраторам");
+//    else
+//      statement.setIcon(new ImageIcon(statementFolder.getAbsolutePath() + '/' + gif_files[0]));
+//
+//    solutionPanel.add(statement, BorderLayout.CENTER);
+//
+//    //create group of panels with radiobuttons
+//    for (int i = 1; i < 6; i++) {
+//      JPanel panel = new JPanel();
+//      selectionPanel.add(panel);
+//      JLabel lab = new JLabel("" + i);
+//      panel.add(lab);
+//      JRadioButton first = new JRadioButton("?");
+//      JRadioButton second = new JRadioButton("!");
+//      JRadioButton third = new JRadioButton("0");
+//      third.setSelected(true);
+//      JRadioButton fourth = new JRadioButton("+");
+//      JRadioButton fifth = new JRadioButton("-");
+//      panel.add(first);
+//      panel.add(second);
+//      panel.add(third);
+//      panel.add(fourth);
+//      panel.add(fifth);
+//      ButtonGroup bg = new ButtonGroup();
+//      bg.add(first);
+//      bg.add(second);
+//      bg.add(third);
+//      bg.add(fourth);
+//      bg.add(fifth);
+//
+//      first.addActionListener(new RadioButtonActionListener(i, first));
+//      second.addActionListener(new RadioButtonActionListener(i, second));
+//      third.addActionListener(new RadioButtonActionListener(i, third));
+//      fourth.addActionListener(new RadioButtonActionListener(i, fourth));
+//      fifth.addActionListener(new RadioButtonActionListener(i, fifth));
+//    }
+//
+//    warningLabel = new JLabel();
+//    resubmitSolutionButton = new JButton();
+//    warningPanel.add(warningLabel, BorderLayout.CENTER);
+//    warningPanel.add(resubmitSolutionButton, BorderLayout.EAST);
+//  }
 
-    //create panels
-    JPanel solutionPanel = new JPanel();
-    JPanel warningPanel = new JPanel();
-    JPanel selectionPanel = new JPanel();
-    mainPanel = new JPanel();
 
-    warningPanel.setVisible(false);
-
-    //set layout for panels
-    mainPanel.setLayout(new BorderLayout());
-    solutionPanel.setLayout(new BorderLayout());
-    warningPanel.setLayout(new BorderLayout());
-    selectionPanel.setLayout(new GridLayout(5, 1));
-
-    //add panels to panels
-    mainPanel.add(solutionPanel, BorderLayout.CENTER);
-    mainPanel.add(warningPanel, BorderLayout.SOUTH);
-    solutionPanel.add(selectionPanel, BorderLayout.SOUTH);
-
-    //fill solution panel
-    JLabel statement = new JLabel();
-
-    File statementFolder = environment.getProblemFolder();
-    String[] gif_files = statementFolder.list(new FilenameFilter() {
-      public boolean accept(File dir, String name) {
-        return name.endsWith(".gif");
-      }
-    });
-
-    if (gif_files == null || gif_files.length == 0)
-      statement.setText("Не удалось загрузить решение, обратитесь к администраторам");
-    else
-      statement.setIcon(new ImageIcon(statementFolder.getAbsolutePath() + '/' + gif_files[0]));
-
-    solutionPanel.add(statement, BorderLayout.CENTER);
-
-    //create group of panels with radiobuttons
-    for (int i = 1; i < 6; i++) {
-      JPanel panel = new JPanel();
-      selectionPanel.add(panel);
-      JLabel lab = new JLabel("" + i);
-      panel.add(lab);
-      JRadioButton first = new JRadioButton("?");
-      JRadioButton second = new JRadioButton("!");
-      JRadioButton third = new JRadioButton("0");
-      third.setSelected(true);
-      JRadioButton fourth = new JRadioButton("+");
-      JRadioButton fifth = new JRadioButton("-");
-      panel.add(first);
-      panel.add(second);
-      panel.add(third);
-      panel.add(fourth);
-      panel.add(fifth);
-      ButtonGroup bg = new ButtonGroup();
-      bg.add(first);
-      bg.add(second);
-      bg.add(third);
-      bg.add(fourth);
-      bg.add(fifth);
-
-      first.addActionListener(new RadioButtonActionListener(i, first));
-      second.addActionListener(new RadioButtonActionListener(i, second));
-      third.addActionListener(new RadioButtonActionListener(i, third));
-      fourth.addActionListener(new RadioButtonActionListener(i, fourth));
-      fifth.addActionListener(new RadioButtonActionListener(i, fifth));
-    }
-
-    warningLabel = new JLabel();
-    resubmitSolutionButton = new JButton();
-    warningPanel.add(warningLabel, BorderLayout.CENTER);
-    warningPanel.add(resubmitSolutionButton, BorderLayout.EAST);
-  }
-
-
-  private class RadioButtonActionListener implements ActionListener {
-
-    private int problemNumber;
-    private JRadioButton button;
-
-    public RadioButtonActionListener(int problemNumber, JRadioButton button) {
-      this.problemNumber = problemNumber;
-      this.button = button;
-    }
-
-    public void actionPerformed(ActionEvent e) {
-      //is called when radio button is pressed
-
-      HashMap<String, String> sol = new HashMap<String, String>();
-      sol.put("action", "answer");
-      sol.put("question", "" + problemNumber);
-      sol.put("answer", button.getText());
-
-      try {
-        HashMap<String, String> ans = environment.submitSolution(sol);
-      } catch (GeneralRequestFailureException ignored) {
-        //do nothing
-      }
-    }
-  }
+//  private class RadioButtonActionListener implements ActionListener {
+//
+//    private int problemNumber;
+//    private JRadioButton button;
+//
+//    public RadioButtonActionListener(int problemNumber, JRadioButton button) {
+//      this.problemNumber = problemNumber;
+//      this.button = button;
+//    }
+//
+//    public void actionPerformed(ActionEvent e) {
+//      //is called when radio button is pressed
+//
+//      HashMap<String, String> sol = new HashMap<String, String>();
+//      sol.put("action", "answer");
+//      sol.put("question", "" + problemNumber);
+//      sol.put("answer", button.getText());
+//
+//      try {
+//        HashMap<String, String> ans = environment.submitSolution(sol);
+//      } catch (GeneralRequestFailureException ignored) {
+//        //do nothing
+//      }
+//    }
+//  }
 }
