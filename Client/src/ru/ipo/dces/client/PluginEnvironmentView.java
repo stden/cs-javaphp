72500cebbcb3bf9cc69a562c6e7affb96788227e
iposov <iposov@7cf9c0ce-a34d-0410-a4ad-6744d85870b3>
2009-07-20 22:28:58 +0000

    the first step to the new interface

--- Client/src/ru/ipo/dces/client/PluginEnvironmentView.java
+++ Client/src/ru/ipo/dces/client/PluginEnvironmentView.java
@@ -1,26 +1,26 @@
-package ru.ipo.dces.client;
-
-import javax.swing.*;
-
-/**
- * Created by IntelliJ IDEA.
- * User: Илья
- * Date: 11.12.2008
- * Time: 15:54:42
- */
-public class PluginEnvironmentView {
-
-  private final JToggleButton button;
-
-  public PluginEnvironmentView() {
-    this.button = new JToggleButton();
-  }
-
-  public JToggleButton getButton() {
-    return button;
-  }
-
-  public void setTitle(String title) {
-    button.setText(title);
-  }
-}
+//package ru.ipo.dces.client;
+//
+//import javax.swing.*;
+//
+///**
+// * Created by IntelliJ IDEA.
+// * User: Илья
+// * Date: 11.12.2008
+// * Time: 15:54:42
+// */
+//public class PluginEnvironmentView {
+//
+//  private final JToggleButton button;
+//
+//  public PluginEnvironmentView() {
+//    this.button = new JToggleButton();
+//  }
+//
+//  public JToggleButton getButton() {
+//    return button;
+//  }
+//
+//  public void setTitle(String title) {
+//    button.setText(title);
+//  }
+//}
