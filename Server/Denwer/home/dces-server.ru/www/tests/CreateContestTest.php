697021015eda0448deb0964dcfffe8f6e304880f
iposov <iposov@7cf9c0ce-a34d-0410-a4ad-6744d85870b3>
2009-08-31 18:47:24 +0000

    started implementing new contest create/edit plugin

---
+++ Server/Denwer/home/dces-server.ru/www/tests/CreateContestTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('DCESTestCase.php');
+
+class CreateContestTestCase extends DCESTestCase
+{
+    public function testEmptyContest()
+    {
+        $this->c->
+    }
+
+}
+
+?>
--- Server/Denwer/home/dces-server.ru/www/tests/constructor.php
+++ Server/Denwer/home/dces-server.ru/www/tests/constructor.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('mock_ups.php');
+require_once('../utils/Messages.php');
 require_once('../requests/CreateDataBase.php');
 require_once('post_request.php');
 
@@ -29,6 +29,71 @@ class Constructor
     {
         return PostRequest::doPostRequest($this->serverURL, 'x='.serialize($reqObj));
     }
+
+    public function connectToContest($login, $password, $contestID = 0)
+    {
+        $request = new ConnectToContestRequest();
+
+        $request->login = $login;
+        $request->password = $password;
+        $request->contestID = $contestID;
+
+        return $this->sendRequestObject($request);
+    }
+
+    public function createContest($rap = 0, $ct = 0, $name = 'name', $descr = 'description', $start = 0, $finish = 0, $regType = 'Self', $userData = array())
+    {
+        $request = new CreateContestRequest();
+
+        /*
+          public int              contestID;
+          public String           name;
+          public String           description;
+          public Date             start;
+          public Date             finish;
+          public RegistrationType registrationType;
+          public UserDataField[]  data;
+          public ResultsAccessPolicy resultsAccessPolicy;
+          public ContestTiming contestTiming;
+         */
+
+        $d = new ContestDescription();
+
+        $d->contestID = -1;
+        $d->name = $name;
+        $d->diption = $descr;
+        $d->start = !$start ? time() : $start;
+        $d->finish = !$finish ? $start + 1*60*60: $finish; //start + 1 hour
+        $d->registrationType = $regType;
+        $d->data = $userData;
+
+        if(!$ct)
+        {
+            $ct = new ContestTiming();
+            $ct->selfContestStart = false;
+            $ct->contestEndingStart = 0;
+            $ct->contestEndingFinish = 0;
+        }
+
+        $d->contestTiming = $ct;
+
+        if(!$rap)
+        {
+            $rap = new ResultsAccessPolicy();
+            $rap->contestPermission = 'FullAccess';
+            $rap->contestEndingPermission = 'FullAccess';
+            $rap->afterContestPermission= 'FullAccess';
+        }
+
+        $d->resultsAccessPolicy = $rap;
+
+        $request->sessionID = $sID; //TODO: add parametr;
+        $request->contest = $d; //TODO: add parametr;
+
+        return $this->sendRequestObject($request);
+    }
+
+
 }
 
 ?>
