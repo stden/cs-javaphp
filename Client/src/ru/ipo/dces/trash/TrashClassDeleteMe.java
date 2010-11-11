29f1d94ea6eb77c7454ef8a63dde344c6fb673ff
iposov <iposov@7cf9c0ce-a34d-0410-a4ad-6744d85870b3>
2010-11-11 11:59:19 +0000

    Test Commit :(:(

---
+++ Client/src/ru/ipo/dces/trash/TrashClassDeleteMe.java
@@ -0,0 +1,12 @@
+package ru.ipo.dces.trash;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: putin
+ * Date: 11.11.2010
+ * Time: 16:00:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class TrashClassDeleteMe {
+
+}
